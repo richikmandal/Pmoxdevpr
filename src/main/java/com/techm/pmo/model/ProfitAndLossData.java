1b070f8e7b4fe093b45dd2ee820bd1ae3889dd97
SD336397 <SD336397@SATBBF-PC00539.gegdc.ad>
2020-05-05 13:05:52 +0530

    Rename P&L

---
+++ src/main/java/com/techm/pmo/model/ProfitAndLossData.java
@@ -0,0 +1,380 @@
+package com.techm.pmo.model;
+
+public class ProfitAndLossData {
+
+  private String projectId;
+  private String ProjectDescription;
+  private String month;
+  private String quarter​;
+  private String fy;
+  private String onHc;
+  private String offHc;
+  private String totalHc;
+  private String eBand;
+  private String hBand;
+  private String mBand;
+  private String tBand;
+  private String p1Band;
+  private String p2Band;
+  private String u1Band;
+  private String u2Band;
+  private String u3Band;
+  private String u4Band;
+  private String uJBand;
+
+  private String revTotal;
+  private String salOnsite;
+  private String salOffshore;
+  private String salaryTotal;
+  private String visaWP;
+  private String travelForeign;
+  private String travelInland;
+  private String subconExpenses;
+  private String subconExpOnsite;
+  private String subconExpOffshore;
+  private String totalSubconExpenses;
+  private String ringFencing;
+  private String projectExpens;
+
+  private String ibuBufferCost;
+  private String ibgBufferCost;
+  private String sbuBufferCost;
+  private String allocDirectCost;
+  private String otherExpense;
+  private String totalDirectCost;
+
+  private String contr;
+  private String sgna;
+  private String ebidta;
+
+  public String getProjectId() {
+    return projectId;
+  }
+
+  public void setProjectId(String projectId) {
+    this.projectId = projectId;
+  }
+
+  public String getProjectDescription() {
+    return ProjectDescription;
+  }
+
+  public void setProjectDescription(String projectDescription) {
+    ProjectDescription = projectDescription;
+  }
+
+  public String getMonth() {
+    return month;
+  }
+
+  public void setMonth(String month) {
+    this.month = month;
+  }
+
+  public String getQuarter​() {
+    return quarter​;
+  }
+
+  public void setQuarter​(String quarter​) {
+    this.quarter​ = quarter​;
+  }
+
+  public String getFy() {
+    return fy;
+  }
+
+  public void setFy(String fy) {
+    this.fy = fy;
+  }
+
+  public String getOnHc() {
+    return onHc;
+  }
+
+  public void setOnHc(String onHc) {
+    this.onHc = onHc;
+  }
+
+  public String getOffHc() {
+    return offHc;
+  }
+
+  public void setOffHc(String offHc) {
+    this.offHc = offHc;
+  }
+
+  public String getTotalHc() {
+    return totalHc;
+  }
+
+  public void setTotalHc(String totalHc) {
+    this.totalHc = totalHc;
+  }
+
+  public String geteBand() {
+    return eBand;
+  }
+
+  public void seteBand(String eBand) {
+    this.eBand = eBand;
+  }
+
+  public String gethBand() {
+    return hBand;
+  }
+
+  public void sethBand(String hBand) {
+    this.hBand = hBand;
+  }
+
+  public String getmBand() {
+    return mBand;
+  }
+
+  public void setmBand(String mBand) {
+    this.mBand = mBand;
+  }
+
+  public String gettBand() {
+    return tBand;
+  }
+
+  public void settBand(String tBand) {
+    this.tBand = tBand;
+  }
+
+  public String getP1Band() {
+    return p1Band;
+  }
+
+  public void setP1Band(String p1Band) {
+    this.p1Band = p1Band;
+  }
+
+  public String getP2Band() {
+    return p2Band;
+  }
+
+  public void setP2Band(String p2Band) {
+    this.p2Band = p2Band;
+  }
+
+  public String getU1Band() {
+    return u1Band;
+  }
+
+  public void setU1Band(String u1Band) {
+    this.u1Band = u1Band;
+  }
+
+  public String getU2Band() {
+    return u2Band;
+  }
+
+  public void setU2Band(String u2Band) {
+    this.u2Band = u2Band;
+  }
+
+  public String getU3Band() {
+    return u3Band;
+  }
+
+  public void setU3Band(String u3Band) {
+    this.u3Band = u3Band;
+  }
+
+  public String getU4Band() {
+    return u4Band;
+  }
+
+  public void setU4Band(String u4Band) {
+    this.u4Band = u4Band;
+  }
+
+  public String getuJBand() {
+    return uJBand;
+  }
+
+  public void setuJBand(String uJBand) {
+    this.uJBand = uJBand;
+  }
+
+  public String getRevTotal() {
+    return revTotal;
+  }
+
+  public void setRevTotal(String revTotal) {
+    this.revTotal = revTotal;
+  }
+
+  public String getSalOnsite() {
+    return salOnsite;
+  }
+
+  public void setSalOnsite(String salOnsite) {
+    this.salOnsite = salOnsite;
+  }
+
+  public String getSalOffshore() {
+    return salOffshore;
+  }
+
+  public void setSalOffshore(String salOffshore) {
+    this.salOffshore = salOffshore;
+  }
+
+  public String getSalaryTotal() {
+    return salaryTotal;
+  }
+
+  public void setSalaryTotal(String salaryTotal) {
+    this.salaryTotal = salaryTotal;
+  }
+
+  public String getVisaWP() {
+    return visaWP;
+  }
+
+  public void setVisaWP(String visaWP) {
+    this.visaWP = visaWP;
+  }
+
+  public String getTravelForeign() {
+    return travelForeign;
+  }
+
+  public void setTravelForeign(String travelForeign) {
+    this.travelForeign = travelForeign;
+  }
+
+  public String getTravelInland() {
+    return travelInland;
+  }
+
+  public void setTravelInland(String travelInland) {
+    this.travelInland = travelInland;
+  }
+
+  public String getSubconExpenses() {
+    return subconExpenses;
+  }
+
+  public void setSubconExpenses(String subconExpenses) {
+    this.subconExpenses = subconExpenses;
+  }
+
+  public String getSubconExpOnsite() {
+    return subconExpOnsite;
+  }
+
+  public void setSubconExpOnsite(String subconExpOnsite) {
+    this.subconExpOnsite = subconExpOnsite;
+  }
+
+  public String getSubconExpOffshore() {
+    return subconExpOffshore;
+  }
+
+  public void setSubconExpOffshore(String subconExpOffshore) {
+    this.subconExpOffshore = subconExpOffshore;
+  }
+
+  public String getTotalSubconExpenses() {
+    return totalSubconExpenses;
+  }
+
+  public void setTotalSubconExpenses(String totalSubconExpenses) {
+    this.totalSubconExpenses = totalSubconExpenses;
+  }
+
+  public String getRingFencing() {
+    return ringFencing;
+  }
+
+  public void setRingFencing(String ringFencing) {
+    this.ringFencing = ringFencing;
+  }
+
+  public String getProjectExpens() {
+    return projectExpens;
+  }
+
+  public void setProjectExpens(String projectExpens) {
+    this.projectExpens = projectExpens;
+  }
+
+  public String getIbuBufferCost() {
+    return ibuBufferCost;
+  }
+
+  public void setIbuBufferCost(String ibuBufferCost) {
+    this.ibuBufferCost = ibuBufferCost;
+  }
+
+  public String getIbgBufferCost() {
+    return ibgBufferCost;
+  }
+
+  public void setIbgBufferCost(String ibgBufferCost) {
+    this.ibgBufferCost = ibgBufferCost;
+  }
+
+  public String getSbuBufferCost() {
+    return sbuBufferCost;
+  }
+
+  public void setSbuBufferCost(String sbuBufferCost) {
+    this.sbuBufferCost = sbuBufferCost;
+  }
+
+  public String getAllocDirectCost() {
+    return allocDirectCost;
+  }
+
+  public void setAllocDirectCost(String allocDirectCost) {
+    this.allocDirectCost = allocDirectCost;
+  }
+
+  public String getOtherExpense() {
+    return otherExpense;
+  }
+
+  public void setOtherExpense(String otherExpense) {
+    this.otherExpense = otherExpense;
+  }
+
+  public String getTotalDirectCost() {
+    return totalDirectCost;
+  }
+
+  public void setTotalDirectCost(String totalDirectCost) {
+    this.totalDirectCost = totalDirectCost;
+  }
+
+  public String getContr() {
+    return contr;
+  }
+
+  public void setContr(String contr) {
+    this.contr = contr;
+  }
+
+  public String getSgna() {
+    return sgna;
+  }
+
+  public void setSgna(String sgna) {
+    this.sgna = sgna;
+  }
+
+  public String getEbidta() {
+    return ebidta;
+  }
+
+  public void setEbidta(String ebidta) {
+    this.ebidta = ebidta;
+  }
+
+
+
+}
