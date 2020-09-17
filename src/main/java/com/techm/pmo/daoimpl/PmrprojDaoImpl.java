82ae7227fb3eed742beb44db4c4b8fa23eb5ff61
SD336397 <SD336397@SATBBF-PC00539.gegdc.ad>
2020-09-17 22:35:17 +0530

    updated prjimpl

--- src/main/java/com/techm/pmo/daoimpl/PmrprojDaoImpl.java
+++ src/main/java/com/techm/pmo/daoimpl/PmrprojDaoImpl.java
@@ -325,7 +325,6 @@ public class PmrprojDaoImpl implements PmrprojDao {
     ProjMap = jdbcMysql.query(getPrjMasterDataFinal, new Object[] {user.getUsername()},
         new PrjMasterMapRowMapper());
     user.setProjMasterData(ProjMap);
-
     return user;
 
   }
